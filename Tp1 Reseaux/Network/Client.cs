561d897fc2530f0ad6afcde8795d72c3bb485844
samueldube <samuel_personnel@outlook.com>
2019-02-22 13:33:32 -0500

    Code Standard

--- Tp1 Reseaux/Network/Client.cs
+++ Tp1 Reseaux/Network/Client.cs
@@ -1,27 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tp1_Reseaux
 {
+	/// <summary>
+	/// Exception thrown when the client cant retrieve the
+	/// player assigned by the server.
+	/// </summary>
+	public class NoPlayerAssignedException : Exception
+	{
+		public NoPlayerAssignedException() { }
+		public NoPlayerAssignedException(string message) : base(message) { }
+	}
+
+	/// <summary>
+	/// Class that handle the client for communicating
+	/// with the server
+	/// </summary>
 	public sealed class Client
 	{
-		public class NoPlayerAssignedException : Exception
-		{
-			public NoPlayerAssignedException() { }
-			public NoPlayerAssignedException(string message) : base(message) { }
-		}
-
-		//Properties of the class
+		//Indicate that the connection is open or close
 		public bool IsOpen => Tcp.Connected;
+
+		//Retrieve the player assigned by the server. SHOULD EXTRACT
 		public int PlayerAssigned { get; private set; }
 
-		//Reader and writer objects used to communicate with the server
+		//Reader used for reading the server stream
 		private static StreamReader Reader = StreamReader.Null;
+
+		//Writer used for sending message to the server
 		private static StreamWriter Writer = StreamWriter.Null;
 
 		//Tcp connexion with the server
@@ -29,12 +37,26 @@ namespace Tp1_Reseaux
 
 		//Instance of the singleton class
 		private static Client Instance = null;
+
+		/// <summary>
+		/// Method that return the instance of the singleton class
+		/// </summary>
+		/// <returns>Instance of the class</returns>
 		public static Client GetInstance() => (Instance is null) ? Instance = new Client() : Instance;
 
+		/// <summary>
+		/// Private constructor for the Client
+		/// </summary>
 		private Client() {
 			PlayerAssigned = default(int);
 		}
 
+		/// <summary>
+		/// Method used to connect the client to the server.
+		/// </summary>
+		/// <param name="server">Ip adress of the server to connect to</param>
+		/// <param name="port">Port number of the server to connect to</param>
+		/// <returns>True if the connection succeded</returns>
 		public bool Connect(string server, int port)
 		{
 			Tcp = new TcpClient();
@@ -49,6 +71,10 @@ namespace Tp1_Reseaux
 			return IsOpen;
 		}
 
+		/// <summary>
+		/// Method used to initiate the connexion with the server. Retrieve
+		/// the reader and writer for communication
+		/// </summary>
 		private void InitClient()
 		{
 			Reader = new StreamReader(Tcp.GetStream());
@@ -63,6 +89,9 @@ namespace Tp1_Reseaux
 			PlayerAssigned = playerAssignedByServer;
 		}
 
+		/// <summary>
+		/// Method used to close the connexion with the Server
+		/// </summary>
 		private void CloseClient()
 		{
 			Reader.Close();
@@ -70,6 +99,10 @@ namespace Tp1_Reseaux
 			Tcp.Close();
 		}
 
+		/// <summary>
+		/// Method used to send request to the server
+		/// </summary>
+		/// <param name="request">Type of request to send</param>
 		public void Send(Request request)
 		{
 			string message = request.ParseSendMessage(request.RawMessage);
@@ -81,6 +114,10 @@ namespace Tp1_Reseaux
 			Writer.Flush();
 		}
 
+		/// <summary>
+		/// Method for readign the stream from the server
+		/// </summary>
+		/// <returns>Stream from the server</returns>
 		public string Read()
 		{
 			return Reader.ReadLine();
