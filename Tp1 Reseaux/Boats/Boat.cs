3aa32840f1585ae592eb15c5daf17ea9b2e7dfc5
Samuel Colassin-Brault <1187247@cstjean.qc.ca>
2019-02-22 14:52:47 -0500

    placer bateaux

--- Tp1 Reseaux/Boats/Boat.cs
+++ Tp1 Reseaux/Boats/Boat.cs
@@ -1,43 +1,44 @@
-﻿namespace Tp1_Reseaux
+﻿
+namespace Tp1_Reseaux
 {
-	/// <summary>
-	/// Enumeration for the different boat possible. Contains
-	/// the life points of the boat as well.
-	/// </summary>
-	public enum BoatType
-	{
-		Torpedo = 3,
-		Submarine = 3,
-		CounterTorpedo = 2,
-		AircraftCarrier = 5,
-		Destroyer= 4
-	}
-
-	/// <summary>
-	/// Class used to handle the boats for the player
-	/// </summary>
-    public class Boat 
+    /// <summary>
+    /// Enumeration for the different boat possible. Contains
+    /// the life points of the boat as well.
+    /// </summary>
+    public enum BoatType
     {
-		//Contains the life points of the boat
-		public int LifePoints { get; set; }
+        Torpedo = 3,
+        Submarine = 3,
+        CounterTorpedo = 2,
+        AircraftCarrier = 5,
+        Destroyer = 4
+    }
 
-		//Indicate if the boat has been placed inside the playable grid
-		public bool IsPlaced { get; set; }
+    /// <summary>
+    /// Class used to handle the boats for the player
+    /// </summary>
+    public class Boat
+    {
+        //Contains the life points of the boat
+        public int LifePoints { get; set; }
 
-		//Indicate that the boat is sunk.
-		public bool Sunk => LifePoints <= 0;
+        //Indicate if the boat has been placed inside the playable grid
+        public bool IsPlaced { get; set; }
 
-		//Type of the current boat
-		public BoatType Type { get; private set; }
+        //Indicate that the boat is sunk.
+        public bool Sunk => LifePoints <= 0;
 
-		/// <summary>
-		/// Constructor for the boat that require the boat type to instantiate
-		/// </summary>
-		/// <param name="type"></param>
-		public Boat(BoatType type)
-		{
-			IsPlaced = false;
-			LifePoints = (int)type;
-		}
-	}
+        //Type of the current boat
+        public BoatType Type { get; private set; }
+
+        /// <summary>
+        /// Constructor for the boat that require the boat type to instantiate
+        /// </summary>
+        /// <param name="type"></param>
+        public Boat(BoatType type)
+        {
+            IsPlaced = false;
+            LifePoints = (int)type;
+        }
+    }
 }
